8fa9909cc24a4cfe1bea5de1f3a6734d457a908b
Chanhee Lee <contact@imch.dev>
2024-05-02 23:48:41 +0900

    Add changeset

--- packages/effect-utils/src/index.ts
+++ packages/effect-utils/src/index.ts
@@ -1,3 +1,4 @@
 export { executeEffect } from './executeEffect.js';
 export { executeStream } from './executeStream.js';
+export { isStream } from './isStream.js';
 export type { InferValueType } from './types.js';
